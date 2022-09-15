eb6bb03f979df73edf4f3b00cfb087c15adcd5d3
Chad Stabler <stablerpsn@gmail.com>
2022-09-15 17:09:27 -0500

    working getById function

--- __tests__/simple-db.test.js
+++ __tests__/simple-db.test.js
@@ -1,5 +1,7 @@
 const fs = require('fs/promises');
 const path = require('path');
+const crypto = require('crypto');
+const SimpleDb = require('../lib/simple-db');
 
 const { CI, HOME } = process.env;
 const BASE_DIR = CI ? HOME : __dirname;
@@ -12,8 +14,16 @@ describe('simple database', () => {
     await fs.mkdir(TEST_DIR, { recursive: true });
   });
 
-  it('needs a first test...', async () => {
+  it('get:id returns object by ID', async () => {
+    const objToFind = {
+      name: 'test',
+      age: 'newBorn'
+    };
+    const id = crypto.randomBytes(8).toString('hex');
 
+    await fs.writeFile(`${TEST_DIR}/${id}.json`, JSON.stringify(objToFind));
+    const db = new SimpleDb(TEST_DIR);
+    expect(await db.get(id)).toEqual(objToFind);
   });
 
 });
